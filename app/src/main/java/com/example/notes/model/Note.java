aed31464c27abfc4f497076f5ee6f60d7bf5d954
jogii <meghana.j@ensatus.in>
2020-04-27 15:05:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notes/model/Note.java
@@ -0,0 +1,19 @@
+package com.example.notes.model;
+
+public class Note {
+
+    private String title;
+    private String content;
+    private String timeStamp;
+
+    public Note(String title, String content, String timeStamp) {
+        this.title = title;
+        this.content = content;
+        this.timeStamp = timeStamp;
+    }
+
+    public Note() {
+    }
+
+
+}
